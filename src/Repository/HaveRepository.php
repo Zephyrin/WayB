1aad11f7082b4758de2935a10fd801802c604515
Flo <damortien@gmail.com>
2020-05-02 15:14:36 +0200

    Update symfony

--- src/Repository/HaveRepository.php
+++ src/Repository/HaveRepository.php
@@ -5,7 +5,7 @@ namespace App\Repository;
 use App\Entity\Have;
 use App\Entity\User;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Symfony\Bridge\Doctrine\RegistryInterface;
+use Doctrine\Persistence\ManagerRegistry;
 
 /**
  * @method Have|null find($id, $lockMode = null, $lockVersion = null)
@@ -15,7 +15,7 @@ use Symfony\Bridge\Doctrine\RegistryInterface;
  */
 class HaveRepository extends ServiceEntityRepository
 {
-    public function __construct(RegistryInterface $registry)
+    public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Have::class);
     }
--- src/Repository/SubCategoryRepository.php
+++ src/Repository/SubCategoryRepository.php
@@ -4,7 +4,7 @@ namespace App\Repository;
 
 use App\Entity\SubCategory;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Symfony\Bridge\Doctrine\RegistryInterface;
+use Doctrine\Persistence\ManagerRegistry;
 
 /**
  * @method SubCategory|null find($id, $lockMode = null, $lockVersion = null)
@@ -14,7 +14,7 @@ use Symfony\Bridge\Doctrine\RegistryInterface;
  */
 class SubCategoryRepository extends ServiceEntityRepository
 {
-    public function __construct(RegistryInterface $registry)
+    public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, SubCategory::class);
     }
