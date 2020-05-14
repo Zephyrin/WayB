293174d1b4f67877451e4b8c8ed51c46955300fa
Flo <damortien@gmail.com>
2020-05-15 00:37:11 +0200

    Remove migration test sqlite

--- src/Migrations/Version20200504051724.php
+++ src/Migrations/Version20200504051724.php
@@ -20,7 +20,7 @@ final class Version20200504051724 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'sqlite', 'Migration can only be executed safely on \'sqlite\'.');
+        // $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'sqlite', 'Migration can only be executed safely on \'sqlite\'.');
 
         $this->addSql('DROP INDEX IDX_BCE3F79812469DE2');
         $this->addSql('CREATE TEMPORARY TABLE __temp__sub_category AS SELECT id, category_id, name FROM sub_category');
