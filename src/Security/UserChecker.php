374e3b395b8504fb63cce2810277bfa14b6cb441
Flo <damortien@gmail.com>
2020-05-25 12:40:06 +0200

    Update checker and remove useless route

--- src/Security/UserChecker.php
+++ src/Security/UserChecker.php
@@ -4,6 +4,10 @@ namespace App\Security;
 use App\Exception\AccountDeletedException;
 use App\Entity\User as AppUser;
 use Symfony\Component\Security\Core\Exception\AccountExpiredException;
+use Symfony\Component\Security\Core\Exception\CredentialsExpiredException;
+use Symfony\Component\Security\Core\Exception\LockedException;
+use Symfony\Component\Security\Core\Exception\DisabledException;
+
 use Symfony\Component\Security\Core\User\UserCheckerInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
@@ -15,11 +19,23 @@ class UserChecker implements UserCheckerInterface
             return;
         }
 
-        // user is deleted, show a generic Account Not Found message.
-        /* if ($user->isDeleted()) {
-            throw new AccountDeletedException();
-        } */
-        throw new AccountDeletedException();
+        if (!$user->isAccountNonLocked()) {
+            $ex = new LockedException('User account is locked.');
+            $ex->setUser($user);
+            throw $ex;
+        }
+
+        if (!$user->isEnabled()) {
+            $ex = new DisabledException('User account is disabled.');
+            $ex->setUser($user);
+            throw $ex;
+        }
+
+        if (!$user->isAccountNonExpired()) {
+            $ex = new AccountExpiredException('User account has expired.');
+            $ex->setUser($user);
+            throw $ex;
+        }
     }
 
     public function checkPostAuth(UserInterface $user)
@@ -32,6 +48,11 @@ class UserChecker implements UserCheckerInterface
         /* if ($user->isExpired()) {
             throw new AccountExpiredException('...');
         } */
+        if (!$user->isCredentialsNonExpired()) {
+            $ex = new CredentialsExpiredException('User credentials have expired.');
+            $ex->setUser($user);
+            throw $ex;
+        }
         throw new AccountExpiredException('...');
     }
 }
